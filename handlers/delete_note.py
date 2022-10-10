ea07d71e6d4664d2c4b70f94b9a4ffdcae137c44
etidart <arsenka12343m@yandex.ru>
2022-10-10 21:47:04 +0300

    Some fixes, now work creating notes

---
+++ handlers/delete_note.py
@@ -0,0 +1,9 @@
+from aiogram import Router, types
+from aiogram.filters.command import Command
+from aiogram.filters.text import Text
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove
+
+router = Router()
+
--- utils/db/db.py
+++ utils/db/db.py
@@ -1,15 +1,16 @@
 import sqlite3
 import json
+import os.path
 from datetime import datetime
 
-con = sqlite3.connect("data.db")
+con = sqlite3.connect('data.db')
 cur = con.cursor()
 
-def add_note_db(user_id, name, content):
+def add_note_db(user_id, title, content):
     now = datetime.now()
     date = "{}.{}.{}".format(now.day, now.month, now.year)
     jarray = json.dumps(content)
 
-    cur.execute("INSERT INTO notes VALUES (?, ?, ?, ?)", [int(user_id), str(name), str(jarray), str(date)])
+    cur.execute("INSERT INTO notes VALUES (?, ?, ?, ?)", (int(user_id), str(title), str(jarray), str(date)))
     con.commit()
 
